1b327d05f26bf81681851a31c069d291127fb322
NguyenLongVu03 <117622104+NguyenLongVu03@users.noreply.github.com>
2023-07-19 18:50:43 +0700

    ..

--- app/src/main/java/com/example/gomraft/LoginActivity.java
+++ app/src/main/java/com/example/gomraft/LoginActivity.java
@@ -8,18 +8,25 @@ import android.view.View;
 import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-    Button btndk;
+    Button btndk, btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         btndk = findViewById(R.id.btnDk);
-
+        btnLogin = findViewById(R.id.btnLogin);
         btndk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(i);
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
             }
         });
     }
