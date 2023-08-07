4ef616e28ddbe4ff021eceeaf02141041df0bb36
tho.nguyen <91511523+haki203@users.noreply.github.com>
2023-08-07 16:02:48 +0700

    Update IRetrofit.java

--- app/src/main/java/com/example/gomraft/Helpers/IRetrofit.java
+++ app/src/main/java/com/example/gomraft/Helpers/IRetrofit.java
@@ -1,12 +1,16 @@
-package com.example.gomraft.Helpers;
+package com.example.gomraft.helpers;
 
-import com.example.gomraft.Dto.ListScheduleSubjectResponseDTO;
+
+import com.example.gomraft.dto.DetailReponseDTO;
+import com.example.gomraft.dto.ListPostsReponseDTO;
 
 import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Query;
+import retrofit2.http.POST;
 
 public interface IRetrofit {
-    @GET("api/get-schedules.php")
-    Call<ListScheduleSubjectResponseDTO> getSchedule(@Query("date") int date,@Query("asc") int asc,  @Query("type") int type, @Query("user_id") int userId);
+
+    @POST("/api/get_all_posts.php")
+    Call<ListPostsReponseDTO> getAllProducts();
+    @POST("/api/get-post-detail.php")
+    Call<DetailReponseDTO> getPostDetail();
 }
